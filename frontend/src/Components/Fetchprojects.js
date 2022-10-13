ce22ead7eb960859cb2d0d3598e407311d5e5541
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-10-13 09:47:25 +0530

    cleaned frontend

---
+++ frontend/src/Components/Fetchprojects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Fetchprojects = () => {
+    return (
+        <div>
+            <input type="link"></input>
+            <input type="link"></input>
+            <div className="Container">
+                Show the Apps
+            </div>
+        </div>
+    )
+}
+
+export default Fetchprojects
